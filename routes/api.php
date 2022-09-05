a2caf10bfea2ebf53b5146d9d6b8e31fdc98dcc4
Jaime Norato <jn901113jn@gmail.com>
2022-09-05 08:24:33 -0500

    * inplementacion de jwt * adicion de endpoinds de autenticacion

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,19 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+Route::group(['middleware' => 'api','prefix'=> 'v1'], function($router){
+    ///Auth
+    Route::group(['prefix' => 'auth'], function ($router) {
+        Route::post('login', [App\Http\Controllers\Api\V1\AuthController::class,"login"]);
+        Route::post('logout', [App\Http\Controllers\Api\V1\AuthController::class,"logout"]);
+        Route::post('refresh', [App\Http\Controllers\Api\V1\AuthController::class,"refresh"]);
+        Route::post('me', [App\Http\Controllers\Api\V1\AuthController::class,"me"]);
+        Route::post('register', [App\Http\Controllers\Api\V1\AuthController::class,"register"]);
+    });
 
-Route::apiResource('v1/invoices', App\Http\Controllers\Api\V1\InvoiceController::class);
-Route::apiResource('v1/invoice/details', App\Http\Controllers\Api\V1\InvoiceDetailController::class);
-Route::apiResource('v1/receivers', App\Http\Controllers\Api\V1\ReceiverController::class)->only(['index', 'store', 'update']);
-Route::apiResource('v1/issuers', App\Http\Controllers\Api\V1\IssuerController::class)->only(['index', 'store', 'update']);
+    Route::apiResource('invoices', App\Http\Controllers\Api\V1\InvoiceController::class);
+    Route::apiResource('invoice/details', App\Http\Controllers\Api\V1\InvoiceDetailController::class);
+    Route::apiResource('receivers', App\Http\Controllers\Api\V1\ReceiverController::class)->only(['index', 'store', 'update']);
+    Route::apiResource('issuers', App\Http\Controllers\Api\V1\IssuerController::class)->only(['index', 'store', 'update']);
+});
+
